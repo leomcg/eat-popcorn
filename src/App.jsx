bd98ba475db2dc81a9d9cf1e585e4b1750d99b3c
leomcg <leomcg@gmail.com>
2026-03-26 16:42:28 -0300

    use localStorage to store watched movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,6 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
-// const tempMovieData = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt0133093",
-//     Title: "The Matrix",
-//     Year: "1999",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt6751668",
-//     Title: "Parasite",
-//     Year: "2019",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-//   },
-// ];
-
-// const tempWatchedData = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//     runtime: 148,
-//     imdbRating: 8.8,
-//     userRating: 10,
-//   },
-//   {
-//     imdbID: "tt0088763",
-//     Title: "Back to the Future",
-//     Year: "1985",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-//     runtime: 116,
-//     imdbRating: 8.5,
-//     userRating: 9,
-//   },
-// ];
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -56,26 +9,14 @@ const KEY = "3f4bce0d";
 export default function App() {
   const [query, setQuery] = useState("god");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedID, setSelectedID] = useState("");
 
-  /*
-  useEffect(() => {
-    console.log("After initial render");
-  }, []);
-
-  console.log("During render");
-
-  useEffect(() => {
-    console.log("D");
-  }, [query]);
-
-  useEffect(() => {
-    console.log("After every render");
+  const [watched, setWatched] = useState(() => {
+    const data = localStorage.getItem("watched");
+    return JSON.parse(data);
   });
-  */
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
@@ -92,6 +33,10 @@ export default function App() {
   function handleCloseMovie() {
     setSelectedID("");
   }
+
+  useEffect(() => {
+    localStorage.setItem("watched", JSON.stringify(watched));
+  }, [watched]);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -308,14 +253,13 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
 
   useEffect(() => {
     const callback = (e) => {
-      if (e.code === 'Escape') onCloseMovie()
-    }
+      if (e.code === "Escape") onCloseMovie();
+    };
 
-    document.addEventListener('keydown', callback)
-    
-    return ()=> document.removeEventListener('keydown', callback)
+    document.addEventListener("keydown", callback);
 
-  }, [onCloseMovie])
+    return () => document.removeEventListener("keydown", callback);
+  }, [onCloseMovie]);
 
   function addWatched() {
     const newMovie = {
