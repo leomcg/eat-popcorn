8db477bf51c5dbc3ca00248f2e5379758e9a2936
leomcg <leomcg@gmail.com>
2026-03-27 16:44:22 -0300

    useKey custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocalStorageState";
+import { useKey } from "./useKey";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -104,23 +105,14 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(() => {
-    const callback = (e) => {
-      // don't clean query if search bar is focused
-      if (document.activeElement === inputEl.current) return;
-
-      if (e.code === "Enter") {
-        inputEl.current.focus();
-        setQuery("");
-      }
-    };
-    document.addEventListener("keydown", callback);
-
-    // focus on initial render
     inputEl.current.focus();
+  }, []);
 
-    // cleanup
-    return () => document.removeEventListener("keydown", callback);
-  }, [setQuery]);
+  useKey("enter", () => {
+    if (document.activeElement === inputEl.current) return;
+    inputEl.current.focus();
+    setQuery("");
+  });
 
   return (
     <input
@@ -244,15 +236,7 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
     };
   }, [title]);
 
-  useEffect(() => {
-    const callback = (e) => {
-      if (e.code === "Escape") onCloseMovie();
-    };
-
-    document.addEventListener("keydown", callback);
-
-    return () => document.removeEventListener("keydown", callback);
-  }, [onCloseMovie]);
+  useKey("escape", onCloseMovie);
 
   function addWatched() {
     const newMovie = {
